43f87462dc46e24460fffc914ce7b62243d90c7e
code-azeemahmad <azeemahmadd9@gmail.com>
2025-09-20 12:05:19 +0500

    Same problem using Alphabets

--- Pattern Problems/Triangle_Pattern_2.cpp
+++ Pattern Problems/Triangle_Pattern_2.cpp
@@ -15,3 +15,19 @@ int main()
 
     return 0;
 }
+
+// int main()
+// {
+//     int n = 5;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         char ch = 'A' + (i - 1);
+//         for (int j = 1; j <= i; j++)
+//         {
+//             cout << ch;
+//         }
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
