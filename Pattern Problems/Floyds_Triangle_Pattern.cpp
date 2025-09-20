2de4b2168977f9c54ce66ecc95bdb754e530036d
code-azeemahmad <azeemahmadd9@gmail.com>
2025-09-20 15:35:00 +0500

    updated Floyd's Triangle

--- Pattern Problems/Floyds_Triangle_Pattern.cpp
+++ Pattern Problems/Floyds_Triangle_Pattern.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 using namespace std;        
                             //  Floyd's Triangle Pattern
-int main()
-{
-    int n = 4;
-    int num = 1;
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = i; j > 0; j--)
-        {
-            cout << num;
-            num++;
-        }
-        cout << endl;
-    }
+// int main()
+// {
+//     int n = 4;
+//     int num = 1;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = i; j > 0; j--)
+//         {
+//             cout << num;
+//             num++;
+//         }
+//         cout << endl;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
 
 // 1
 // 23
@@ -24,24 +24,22 @@ int main()
 // 78910
 
 
-// int main()
-// {
-//     int n = 4;
-//     int num = 0;
-//     for (int i = 1; i <= n; i++)
-//     {
-//         char ch = 'A' + num;
-//         for (int j = i; j > 0; j--)
-//         {
-//             cout << ch;
-//             num++;
-//             ch++;
-//         }
-//         cout << endl;
-//     }
+int main()
+{
+    int n = 4;
+    char ch = 'A';
+    for (int i = 1; i <= n; i++)
+    { 
+        for (int j = i; j > 0; j--)
+        {
+            cout << ch;
+            ch++;
+        }
+        cout << endl;
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
 // A
 // BC
