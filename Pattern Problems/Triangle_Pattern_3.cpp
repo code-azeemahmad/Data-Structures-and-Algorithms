61fe811bd577ee719fb87a08cf6aa9cbdac14432
code-azeemahmad <azeemahmadd9@gmail.com>
2025-09-20 12:17:55 +0500

    Triangle Pattern with increasing Number

---
+++ Pattern Problems/Triangle_Pattern_3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+// int main()
+// {
+//     int n = 5;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         char ch = 'A';
+//         for (int j = 1; j <= i; j++)
+//         {
+//             cout << ch;
+//             ch +=1;
+//         }
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
