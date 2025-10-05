f7d82aa19670aad0e5882a8ed00cb79e8f10b24b
code-azeemahmad <azeemahmadd9@gmail.com>
2025-10-05 11:51:18 +0500

    Null pointers in cpp

---
+++ Pointers/NULL_pointer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int *ptr;
+    int *Nptr = NULL;
+
+    cout << ptr << endl;
+    cout << Nptr;
+
+    return 0;
+}
+
+/*A pointer that doesn't point to any location.*/
+/*Correct usage of null pointer in linked list and trees*/
