e5c323265edd696acf6362b59b8c8a1d7cb44f8c
code-azeemahmad <azeemahmadd9@gmail.com>
2025-10-05 19:29:50 +0500

    Use of Pointer arithmetic in accessing array elements

--- Pointers/PointerArithmetic.cpp
+++ Pointers/PointerArithmetic.cpp
@@ -10,8 +10,14 @@ int main()
     cout << ptr << endl;
 
     ptr = ptr + 2;          // store 8 bytes more
-    
     cout << ptr << endl;
+
+    int arr[] = {1, 2, 3, 4, 5};
+    cout << *arr << endl;
+    cout << *(arr + 1) << endl;
+    cout << *(arr + 2) << endl;
+    cout << *(arr + 3) << endl;
+    cout << *(arr + 4) << endl;
 
     return 0;
 }
@@ -19,4 +25,10 @@ int main()
 /*
 ptr++ --> adds memory of 4 bytes = one integar for int variable to a pointer
 0x61fe14 --> +4 bytes --> 0x61fe18
+*/
+
+/*
+array memory address are contiguous
+        1   2   3   4   5
+       100 104 108 112 116
 */
