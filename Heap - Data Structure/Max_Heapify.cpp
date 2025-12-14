ee7830750a99438fee0c9475c116fe990571ff7b
code-azeemahmad <azeemahmadd9@gmail.com>
2025-12-14 08:28:57 +0500

    Create Max_Heapify.cpp
    Building max heap using Max Heapify (Heapify Down)

---
+++ Heap - Data Structure/Max_Heapify.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void maxHeapify(int arr[], int n, int i)
+{
+    int largest = i;       // Initialize largest as root
+    int left = 2 * i + 1;  // left = 2*i + 1
+    int right = 2 * i + 2; // right = 2*i + 2
+
+    // If left child is larger than root
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    // If right child is larger than largest so far
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    // If largest is not root
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+
+        // Recursively heapify the affected sub-tree
+        maxHeapify(arr, n, largest);
+    }
+}
+
+int main()
+{
+    int arr[7] = {15, 5, 20, 1, 17, 10, 30};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int lastLeafNode = (n / 2) - 1;
+
+    for (int i = lastLeafNode; i >= 0; i--)
+    {
+        maxHeapify(arr, n, i);
+    }
+    cout << "Max-Heap array: ";
+    for (int i = 0; i < n; ++i)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
